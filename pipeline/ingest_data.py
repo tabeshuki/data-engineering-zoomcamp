47a712be5cf932e68cf279705d6a8a2fc4c96580
tabeshuki <ishikawashuka@gmail.com>
2026-01-26 20:48:07 +0000

    Homework changes

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -5,7 +5,6 @@
 
 import pandas as pd
 from sqlalchemy import create_engine
-from tqdm.auto import tqdm
 import click
 
 dtype = {
@@ -22,14 +21,16 @@ dtype = {
     "mta_tax": "float64",
     "tip_amount": "float64",
     "tolls_amount": "float64",
+    "ehail_fee": "float64",
     "improvement_surcharge": "float64",
     "total_amount": "float64",
+    "trip_type": "Int64",
     "congestion_surcharge": "float64"
 }
 
 parse_dates = [
-    "tpep_pickup_datetime",
-    "tpep_dropoff_datetime"
+    "lpep_pickup_datetime",
+    "lpep_dropoff_datetime"
 ]
 
 @click.command()
@@ -38,31 +39,27 @@ parse_dates = [
 @click.option('--pg-host', default='localhost', help='PostgreSQL host')
 @click.option('--pg-port', type=int, default=5432, help='PostgreSQL port')
 @click.option('--pg-db', default='ny_taxi', help='PostgreSQL database')
-@click.option('--table', default='yellow_taxi_data', help='Table name to ingest data into')
-@click.option('--year', type=int, default=2021, help='Year of data')
-@click.option('--month', type=int, default=1, help='Month of data')
+@click.option('--table', default='green_taxi_data', help='Table name to ingest data into')
+@click.option('--year', type=int, default=2025, help='Year of data')
+@click.option('--month', type=int, default=11, help='Month of data')
 @click.option('--chunksize', type=int, default=100000, help='Chunk size for reading CSV')
 def run(pg_user, pg_pass, pg_host, pg_port, pg_db, table, year, month, chunksize):
-    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+    url = 'green_tripdata_2025-11.parquet'
 
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
-    df_iter = pd.read_csv(
-        url,
-        dtype=dtype,
-        parse_dates=parse_dates,
-        iterator=True,
-        chunksize=chunksize
-    )
+    df = pd.read_parquet(url)
     
-    first = True
-    for df_chunk in tqdm(df_iter):
-        print(f'Ingesting chunk...')
-        if first:
-            df_chunk.head(n=0).to_sql(name=table, con=engine, if_exists='replace')
-            first = False
-        df_chunk.to_sql(name=table, con=engine, if_exists='append')
+    # Apply dtypes
+    df = df.astype(dtype)
+    
+    print(f'Ingesting green taxi data...')
+    df.to_sql(name=table, con=engine, if_exists='replace')
+    
+    # Ingest taxi zones
+    zones_df = pd.read_csv('taxi_zone_lookup.csv')
+    print(f'Ingesting taxi zones...')
+    zones_df.to_sql(name='taxi_zones', con=engine, if_exists='replace')
 
 if __name__ == '__main__':
     run()
